ed6cf2ff2c9fde4701c814033d64d1a34e53d504
Henrique Ferreira <henriquejoaof@gmail.com>
2013-08-17 22:48:57 +0000

    change fliesync and more

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 var buf = fs.readFileSync('index.html');
-var finalString = buf.toString('utf-8',0,buf.lenght-1);
+var finalString = buf.toStringr;
 
 app.get('/', function(request, response){
   response.send(finalString);
